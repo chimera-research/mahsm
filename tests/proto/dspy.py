ab75a14d725c9e2bb153e51bb3076751b6148dff
Sohum-Prime <sohumhk04@gmail.com>
2025-10-31 16:13:56 -0400

    remove API key hardcoding

--- tests/proto/dspy.py
+++ tests/proto/dspy.py
@@ -1,5 +1,5 @@
 import dspy
-lm = dspy.LM("openai/gpt-4o-mini", api_key="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")
+lm = dspy.LM("openai/gpt-4o-mini", api_key=os.getenv("OPENAI_API_KEY"))
 dspy.configure(lm=lm)
 
 math = dspy.ChainOfThought("question -> answer: float")
--- tests/proto/graph.py
+++ tests/proto/graph.py
@@ -10,7 +10,7 @@ class State(TypedDict):
 
 graph_builder = StateGraph(State)
 
-llm = ChatOpenAI(model = "gpt-4o", api_key="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", temperature = 0.2)
+llm = ChatOpenAI(model = "gpt-4o", api_key=os.getenv("OPENAI_API_KEY"), temperature = 0.2)
 
 # The chatbot node function takes the current State as input and returns an updated messages list. This is the basic pattern for all LangGraph node functions.
 def chatbot(state: State):
